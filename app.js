2ac485cf100859df8aa9ecec5a2361e7dff8df45
AustroLupus <89206747+AustroLupus@users.noreply.github.com>
2025-01-31 01:59:41 -0300

    Made functional add name button

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayNombres = []
 function agregarAmigo(){
-    console.log("Agregar Amigo")
+    //console.log("Agregar Amigo")
+    let nuevoNombre = document.getElementById("amigo").value;
+    console.log("Nuevo Nombre: " + nuevoNombre);
+    arrayNombres.push(nuevoNombre);
+    document.getElementById("amigo").value = "";
+    console.log(arrayNombres);
+    let li = document.createElement("li");
+    li.textContent=nuevoNombre;
+    document.getElementById("listaAmigos").appendChild(li);
 }
 
 function sortearAmigo(){
