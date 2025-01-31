4943b95b913e80ce4e28074921fb12079a18cf74
AustroLupus <89206747+AustroLupus@users.noreply.github.com>
2025-01-31 02:11:21 -0300

    Retrieving names from array in console, pending validations

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayNombres = []
 function agregarAmigo(){
-    //console.log("Agregar Amigo")
     let nuevoNombre = document.getElementById("amigo").value;
     console.log("Nuevo Nombre: " + nuevoNombre);
     arrayNombres.push(nuevoNombre);
     document.getElementById("amigo").value = "";
     console.log(arrayNombres);
+    // Mostrando nombres en el listado
     let li = document.createElement("li");
     li.textContent=nuevoNombre;
     document.getElementById("listaAmigos").appendChild(li);
 }
 
 function sortearAmigo(){
-    console.log("Click en Sorteo")
+    let indiceAleatorio = Math.floor(Math.random() * arrayNombres.length);
+    console.log(indiceAleatorio);
+    console.log(arrayNombres[indiceAleatorio]);
 }
